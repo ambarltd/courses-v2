d12643afcb1419803c414adfcb0ab945c3560c61
Luis Galeas <galeaspablo@hotmail.com>
2024-09-16 18:06:43 +0100

    Update code

--- infrastructure/production.tf
+++ infrastructure/production.tf
@@ -7,7 +7,7 @@ module "production_monolith" {
   environment_uuid           = "a${random_id.production_uuid.hex}-pro"
   service_name               = "mon"
   git_commit_hash            = var.git_commit_hash
-  pgt_proxy_cert_common_name = local.credentials["pgtproxy_cert_common_name"]
+  pgt_proxy_cert_common_name = local.credentials["pgt_proxy_certificate_common_name"]
   pgtproxy_cert_in_base64    = local.credentials["pgtproxy_cert_in_base64"]
   pgtproxy_key_in_base64     = local.credentials["pgtproxy_key_in_base64"]
   application_directory_name = "monolith"
