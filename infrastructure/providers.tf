b4a983ec647c268413acee0490272c4bec9e4586
Luis Galeas <galeaspablo@hotmail.com>
2024-09-17 16:49:17 +0100

    Add Ambar

--- infrastructure/providers.tf
+++ infrastructure/providers.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     ambar = {
       source  = "ambarltd/ambar"
-      version = "1.0.6"
+      version = "1.0.10"
     }
     google = {
       source  = "hashicorp/google"
@@ -13,6 +13,12 @@ terraform {
   backend "s3" {
     encrypt = true
   }
+}
+
+provider "ambar" {
+  api_key = base64decode(local.credentials["ambar_api_key"])
+  endpoint   = "euw1.api.ambar.cloud"
+  alias      = "production"
 }
 
 provider "google" {
