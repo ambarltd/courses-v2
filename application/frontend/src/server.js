e856720e220e0496d869c8e3720c436f0d2d0f75
Marcelo Lazaroni <lazaronijunior@gmail.com>
2024-09-17 13:37:02 +0100

    Use templating

--- application/frontend/src/server.js
+++ application/frontend/src/server.js
@@ -1,26 +1,36 @@
 import Express from "express";
 import fetch from "node-fetch"
+import { engine } from 'express-handlebars';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
-const port = 3000
+const port = 8080
 const app = Express()
 
 // Accept JSON bodies
 app.use(Express.json())
 app.use(Express.urlencoded({ extended: true }));
 
-// Serve files in /public
-app.use(Express.static('public'))
+// Setup templating
+const __dirname = dirname(fileURLToPath(import.meta.url));
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', `${__dirname}/views`);
 
 app.get('/sign-in', (_, res) => {
-  res.sendFile(`${process.cwd()}/public/sign-in.html`)
+  res.render(`sign-in`, { layout: false })
 })
 
 app.get('/sign-up', (_, res) => {
-  res.sendFile(`${process.cwd()}/public/sign-up.html`)
+  res.render(`sign-up`, { layout: false })
 })
 
 app.get('/home', (_, res) => {
-  res.sendFile(`${process.cwd()}/public/home.html`)
+  res.render(`home`, { layout: false })
+})
+
+app.get('/', (_, res) => {
+  res.render(`index`, { layout: false })
 })
 
 app.post('/sign-in', async (req, res) => {
@@ -43,13 +53,21 @@ app.post('/sign-in', async (req, res) => {
       headers: {'Content-Type': 'application/json'}
   });
 
+  const r = await response.json();
   if (!response.ok) {
-    console.log(response);
-    res.send("Something went wrong")
+    const rawError = getError(r);
+    const error =
+      rawError == "Security_Session_SignIn_UserNotFound"
+      ? "Invalid email or password"
+      : rawError
+
+    res.render("sign-in", {
+      layout: false,
+      error,
+    });
     return;
   }
 
-  const r = response.json()
   if (typeof r.sessionTokenCreated === "string") {
     console.log(`Success. Session token: ${r.sessionTokenCreated}`);
     res.redirect(`/home?session-id=${sessionTokenCreated}`);
@@ -58,6 +76,17 @@ app.post('/sign-in', async (req, res) => {
     res.send(`Failure. ${JSON.stringify(r)}`);
   }
 });
+
+// Parse error from a failure response
+function getError({ errors, errorIdentifier, errorMessage }) {
+  return (errors.length > 0 && errorMessage.length > 0)
+    ? `${errorMessage}: ${errors.join(". ")}`
+    : errorMessage.length > 0
+    ? errorMessage
+    : errors.length > 0
+    ? errors[0]
+    : errorIdentifier
+}
 
 app.post('/sign-up', async (req, res) => {
   const { email, password, username } = req.body;
@@ -86,9 +115,18 @@ app.post('/sign-up', async (req, res) => {
   const r = await response.json()
 
   if (!response.ok) {
-    // const { errors, errorMessage } = r;
-    console.log("Errors: ", r);
-    res.json(r);
+    const rawError = getError(r);
+    const error =
+      rawError == "Identity_User_SignUp_InvalidPassword"
+      ? "Invalid password. A password must have between 10 and 64 characters and contain a number, an upper case letter, a lower case letter and a special character."
+      : rawError;
+
+    res.render("sign-up", {
+      layout: false,
+      error,
+      username,
+      email,
+    });
     return;
   }
 
