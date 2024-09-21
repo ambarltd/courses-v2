f3669fa276d17e65dc020e904739c42bffef1a4f
Luis Galeas <galeaspablo@hotmail.com>
2024-09-21 03:57:58 +0100

    Fix names

--- infrastructure/ambar/production/data_destination_identity_user.tf
+++ infrastructure/ambar/production/data_destination_identity_user.tf
@@ -8,11 +8,11 @@ resource "ambar_data_destination" "identity_event_store_proxy" {
   password             = "password"
 }
 
-resource "ambar_data_destination" "identity_event_store_proxy" {
+resource "ambar_data_destination" "identity_event_store_direct" {
   filter_ids = [
     ambar_filter.identity_all.resource_id,
   ]
-  description          = "identity_event_store_proxy"
+  description          = "identity_event_store_direct"
   destination_endpoint = "https://webhook.site/94a5b769-bc57-435f-9f67-a9f6d799f882"
   username             = "username"
   password             = "password"
