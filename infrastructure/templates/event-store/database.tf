67f36cbcac8e722d4dd77fe8f614f52c4735fc74
Luis Galeas <galeaspablo@hotmail.com>
2024-09-21 05:11:37 +0100

    Restart everything

--- infrastructure/templates/event-store/database.tf
+++ infrastructure/templates/event-store/database.tf
@@ -9,7 +9,7 @@ resource "google_sql_database" "main" {
 
 resource "google_sql_database_instance" "main" {
   project             = local.gcp_default_project
-  name                = "${var.resource_id_prefix}-i1"
+  name                = "${var.resource_id_prefix}-ii"
   database_version    = "POSTGRES_15"
   region              = local.gcp_default_region
   deletion_protection = false
@@ -55,7 +55,7 @@ resource "google_sql_database_instance" "main" {
 
     database_flags {
       name  = "cloudsql.enable_pglogical"
-      value = "off"
+      value = "on"
     }
 
     database_flags {
