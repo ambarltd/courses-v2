ee36cea94bbf681c84f0ecc7b88f8f897bdcfaa0
Luis Galeas <galeaspablo@hotmail.com>
2024-09-23 04:22:37 +0100

    Begin doing type checks

--- application/backend-monorepo-multiservice/code/dev_type_check.sh
+++ application/backend-monorepo-multiservice/code/dev_type_check.sh
@@ -3,6 +3,4 @@ set -e
 
 cd development-environment
 echo "PHPStan for /srv/src"
-docker exec -it php-all-services-test /srv/vendor/bin/phpstan analyse /srv/src --level max --no-interaction -c phpstan.src.neon --verbose
-echo "PHPStan for /srv/tests"
-docker exec -it php-all-services-test /srv/vendor/bin/phpstan analyse /srv/tests --level max --no-interaction -c phpstan.tests.neon --verbose
+docker exec -it php-all-services-test "php" "vendor/bin/phpstan" "analyse" "--level" "max" "-c" "phpstan.src.neon" "-vvv"
