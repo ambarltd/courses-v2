cb395b18c8afb9dcd84a176b592ca0b7fe9fb163
Marcelo Lazaroni <lazaronijunior@gmail.com>
2024-09-24 14:43:34 +0100

    Add user details page

--- application/frontend/src/main.js
+++ application/frontend/src/main.js
@@ -19,7 +19,7 @@ const endpoints = {
   "verify-primary-email": domains.identity + "/user/verify-primary-email",
   "refresh-token": domains.security + "/session/refresh-token",
   "sign-in": domains.security + "/session/sign-in",
-  "sign-out": domains.security + "session/sign-out"
+  "sign-out": domains.security + "/session/sign-out"
 }
 
 // Accept JSON bodies
@@ -71,9 +71,9 @@ app.set('views', `${__dirname}/views`);
 app.get('/sign-in', unauthenticated, render("sign-in"))
 app.get('/sign-up', unauthenticated, render("sign-up"))
 app.get('/sign-up-success', unauthenticated, render("sign-up-success"))
-app.get('/home', /*authenticated,*/ render("home", "main"))
+app.get('/user/details', /*authenticated,*/ render("details", "main"))
 app.get('/logout', authenticated, routeLogout)
-app.get('/', authenticated, render("home"))
+app.get('/', authenticated, render("home", "main"))
 app.get('/verify-email', unauthenticated, routeVerifyEmail);
 app.post('/sign-in', unauthenticated, routeSignIn);
 app.post('/sign-up', unauthenticated,  routeSignUp);
@@ -111,6 +111,7 @@ async function routeVerifyEmail(req,res) {
 
 async function routeSignIn(req, res) {
   const { email, password } = req.body;
+
   const contents = {
     withUsernameOrEmail: email,
     withPassword: password,
@@ -141,7 +142,7 @@ async function routeSignIn(req, res) {
   const token = r.sessionTokenCreated;
   if (typeof token === "string") {
     authenticate(req, token);
-    res.redirect("/home");
+    res.redirect("/");
     return;
   } else {
     res.send(`Failure. ${JSON.stringify(r)}`);
