1cd209b72f1e55166acbd2205169c3f0b52ca092
Luis Galeas <galeaspablo@hotmail.com>
2024-09-25 13:48:57 +0100

    Wait fr containers to start

--- application/backend-monorepo-multiservice/code/development-environment/dev_a_start.sh
+++ application/backend-monorepo-multiservice/code/development-environment/dev_a_start.sh
@@ -6,9 +6,15 @@ docker run --rm -v $(pwd):/app composer:2.2.24 install --ignore-platform-reqs
 cd development-environment
 docker compose down
 docker compose up -d --build --force-recreate
-docker ps
+echo "Waiting for containers to start"
 sleep 5
 docker ps
-sleep 5
-docker ps
-sleep 5
+
+
+if [ "$(docker ps -q -f name=php-all-services-test)" ]; then
+    echo "The container 'php-all-services-test' is up and running."
+    docker logs php-all-services-test | tail -n 5
+else
+    echo "The container 'php-all-services-test' is not running."
+    docker logs php-all-services-test | tail -n 500
+fi
