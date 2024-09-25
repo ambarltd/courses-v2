4b1a3969cbd70408ce755f19192ebdb47b59fea9
Luis Galeas <galeaspablo@hotmail.com>
2024-09-26 00:55:52 +0100

    Query Handler Tests in User

---
+++ application/backend-monorepo-multiservice/code/tests/UnitAndIntegration/BoundedContext/Identity/User/QueryHandler/GetUserDetailsQueryHandlerTest.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Galeas\Api\UnitAndIntegration\BoundedContext\Identity\User\QueryHandler;
+
+use Galeas\Api\BoundedContext\Identity\User\Projection\UserDetails\GetUserDetails;
+use Galeas\Api\BoundedContext\Identity\User\Query\GetUserDetailsQuery;
+use Galeas\Api\BoundedContext\Identity\User\QueryHandler\GetUserDetailsQueryHandler;
+use PHPUnit\Framework\Assert;
+use Tests\Galeas\Api\UnitAndIntegration\HandlerUnitTest;
+
+class GetUserDetailsQueryHandlerTest extends HandlerUnitTest
+{
+    public function testHandle(): void
+    {
+        $query = new GetUserDetailsQuery();
+        $query->authenticatedUserId = 'user_id_1';
+
+        $getUserDetails = $this->mockForCommandHandlerWithReturnValue(
+            GetUserDetails::class,
+            'getUserDetails',
+            [
+                'userId' => 'user_id_1',
+                'primaryEmailStatus' => [
+                    'verifiedEmail' => [
+                        'email' => 'email_address_1',
+                    ],
+                ],
+            ]
+        );
+        Assert::assertEquals(
+            [
+                'userId' => 'user_id_1',
+                'primaryEmailStatus' => [
+                    'verifiedEmail' => [
+                        'email' => 'email_address_1',
+                    ],
+                ],
+            ],
+            (new GetUserDetailsQueryHandler($getUserDetails))->handle($query)
+        );
+    }
+}
---
+++ application/backend-monorepo-multiservice/code/tests/UnitAndIntegration/BoundedContext/Identity/User/QueryHandler/ListSentVerificationEmailQueryHandlerTest.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Galeas\Api\UnitAndIntegration\BoundedContext\Identity\User\QueryHandler;
+
+use Galeas\Api\BoundedContext\Identity\User\Projection\SentVerificationEmailV2\ListSentVerificationEmail;
+use Galeas\Api\BoundedContext\Identity\User\Query\ListSentVerificationEmailQuery;
+use Galeas\Api\BoundedContext\Identity\User\QueryHandler\ListSentVerificationEmailQueryHandler;
+use PHPUnit\Framework\Assert;
+use Tests\Galeas\Api\UnitAndIntegration\HandlerUnitTest;
+
+class ListSentVerificationEmailQueryHandlerTest extends HandlerUnitTest
+{
+    public function testHandle(): void
+    {
+        $query = new ListSentVerificationEmailQuery();
+
+        $listSentVerificationEmail = $this->mockForCommandHandlerWithReturnValue(
+            ListSentVerificationEmail::class,
+            'list',
+            ['foo' => 'bar']
+        );
+        Assert::assertEquals(
+            [
+                'foo' => 'bar',
+            ],
+            (new ListSentVerificationEmailQueryHandler($listSentVerificationEmail))->handle($query)
+        );
+    }
+}
