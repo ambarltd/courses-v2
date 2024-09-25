afdd839f0c658ab26dd505370c77287982dd5a48
Luis Galeas <galeaspablo@hotmail.com>
2024-09-25 13:29:58 +0100

    Fixed memory limits, plus longer wait on container start

--- application/backend-monorepo-multiservice/code/development-environment/dev_a_start.sh
+++ application/backend-monorepo-multiservice/code/development-environment/dev_a_start.sh
@@ -6,4 +6,9 @@ docker run --rm -v $(pwd):/app composer:2.2.24 install --ignore-platform-reqs
 cd development-environment
 docker compose down
 docker compose up -d --build --force-recreate
+docker ps
+sleep 5
+docker ps
+sleep 5
+docker ps
 sleep 5
