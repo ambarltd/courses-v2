bad777dd8113c18226e34944369b5ca96fc1261f
Luis Galeas <galeaspablo@hotmail.com>
2024-09-26 02:27:26 +0100

    Push fixes and new projections

---
+++ infrastructure/ambar/production/data_destination_creditcardproduct_product.tf
@@ -0,0 +1,9 @@
+resource "ambar_data_destination" "CreditCardProduct_Product_ProductListItem" {
+  filter_ids = [
+    ambar_filter.credit_card_product.resource_id,
+  ]
+  description          = "CreditCardProduct_Product_ProductListItem"
+  destination_endpoint = "${var.data_destination_security.endpoint_prefix}/api/v1/credit_card_product/product/projection/product_list_item"
+  username             = "username"
+  password             = "password"
+}
