0d8bfcc21f218103969aa591859e41f48bcf45a2
Luis Galeas <galeaspablo@hotmail.com>
2024-09-26 03:23:08 +0100

    Correct endpoint for cc product

--- infrastructure/ambar/production/data_destination_creditcardproduct_product.tf
+++ infrastructure/ambar/production/data_destination_creditcardproduct_product.tf
@@ -3,7 +3,7 @@ resource "ambar_data_destination" "CreditCardProduct_Product_ProductListItem" {
     ambar_filter.credit_card_product.resource_id,
   ]
   description          = "CreditCardProduct_Product_ProductListItem"
-  destination_endpoint = "${var.data_destination_security.endpoint_prefix}/api/v1/credit_card_product/product/projection/product_list_item"
+  destination_endpoint = "${var.data_destination_credit_card_product.endpoint_prefix}/api/v1/credit_card_product/product/projection/product_list_item"
   username             = "username"
   password             = "password"
 }
