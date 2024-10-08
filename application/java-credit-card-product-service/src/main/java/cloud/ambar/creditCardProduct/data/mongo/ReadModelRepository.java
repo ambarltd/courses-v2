2e3124dba622f50fb1bbe524779039404b8604af
Tom Schutte <tom@ambar.cloud>
2024-10-08 12:39:45 -0500

    Update getItem method signature

--- application/java-credit-card-product-service/src/main/java/cloud/ambar/creditCardProduct/data/mongo/ReadModelRepository.java
+++ application/java-credit-card-product-service/src/main/java/cloud/ambar/creditCardProduct/data/mongo/ReadModelRepository.java
@@ -4,5 +4,5 @@ import cloud.ambar.creditCardProduct.models.projection.ProductListItem;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface ReadModelRepository extends MongoRepository<ProductListItem, String> {
-    ProductListItem getItem(String aggregateId);
+    ProductListItem getItem(String id);
 }
