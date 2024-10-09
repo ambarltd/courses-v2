2c342c3891b20b705924b7e6d44789f066467a77
Tom Schutte <tom@ambar.cloud>
2024-10-09 16:39:42 -0500

    I am a very silly man

--- application/java-credit-card-product-service/src/main/java/cloud/ambar/creditCardProduct/data/models/projection/Product.java
+++ application/java-credit-card-product-service/src/main/java/cloud/ambar/creditCardProduct/data/models/projection/Product.java
@@ -5,12 +5,12 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.Column;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
-@AllArgsConstructor
+@NoArgsConstructor
 @Document(collection = "ProductListItems")
 @Table(name = "ProductListItems")
 @JsonIgnoreProperties(ignoreUnknown = true)
