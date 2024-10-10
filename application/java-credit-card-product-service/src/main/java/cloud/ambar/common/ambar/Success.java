579916e2f177d85337fbf2cea8c12f72901a1db4
Tom Schutte <tom@ambar.cloud>
2024-10-09 20:58:07 -0500

    Give success a json body

--- application/java-credit-card-product-service/src/main/java/cloud/ambar/common/ambar/Success.java
+++ application/java-credit-card-product-service/src/main/java/cloud/ambar/common/ambar/Success.java
@@ -1,7 +1,17 @@
 package cloud.ambar.common.ambar;
 
 
-public class Success {
+import com.fasterxml.jackson.databind.JsonNode;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Success {
+    private JsonNode node;
 }
 
