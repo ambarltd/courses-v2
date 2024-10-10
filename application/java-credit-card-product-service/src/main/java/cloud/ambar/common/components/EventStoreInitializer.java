78ed99f91171cd629d1a3ef8b9a8cfe4f6d6cb63
Tom Schutte <tom@ambar.cloud>
2024-10-09 21:10:28 -0500

    Cleanup app props and logging

--- application/java-credit-card-product-service/src/main/java/cloud/ambar/common/components/EventStoreInitializer.java
+++ application/java-credit-card-product-service/src/main/java/cloud/ambar/common/components/EventStoreInitializer.java
@@ -127,9 +127,6 @@ public class EventStoreInitializer {
         } catch (Exception e) {
             log.warn("Caught exception when executing SQL statement.");
             log.warn(e);
-            log.warn(e.getMessage());
-            log.warn(e.getCause());
-            log.warn(e.getStackTrace());
         }
     }
 }
