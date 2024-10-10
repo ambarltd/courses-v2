eac27d11e6d15a737278bf4314ef51716ade7484
Tom Schutte <tom@ambar.cloud>
2024-10-09 20:23:38 -0500

    Try auth source in URI

--- application/java-credit-card-product-service/src/main/java/cloud/ambar/creditCardProduct/data/models/projection/Product.java
+++ application/java-credit-card-product-service/src/main/java/cloud/ambar/creditCardProduct/data/models/projection/Product.java
@@ -2,9 +2,7 @@ package cloud.ambar.creditCardProduct.data.models.projection;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import jakarta.persistence.Column;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -12,16 +10,11 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Data
 @NoArgsConstructor
 @Document(collection = "ProductListItems")
-@Table(name = "ProductListItems")
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Product {
     @Id
     @JsonProperty("aggregate_id")
     private String id;
-
-    @Column(name = "name")
     private String name;
-
-    @Column(name = "active")
     private boolean active;
 }
