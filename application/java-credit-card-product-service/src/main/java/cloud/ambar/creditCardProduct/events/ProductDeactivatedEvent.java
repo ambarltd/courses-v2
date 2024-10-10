5ad02dade04146e664879c59e84d4bd0688f0de7
Tom Schutte <tom@ambar.cloud>
2024-10-09 22:11:07 -0500

    Cleanup dependencies and unused imports

--- application/java-credit-card-product-service/src/main/java/cloud/ambar/creditCardProduct/events/ProductDeactivatedEvent.java
+++ application/java-credit-card-product-service/src/main/java/cloud/ambar/creditCardProduct/events/ProductDeactivatedEvent.java
@@ -1,6 +1,5 @@
 package cloud.ambar.creditCardProduct.events;
 
-import cloud.ambar.creditCardProduct.aggregate.IEventTransformProduct;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
