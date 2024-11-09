fd8abd3a97c609cddff1fcd1ca4daec884ba8386
Luis Galeas <galeaspablo@hotmail.com>
2024-11-09 01:45:29 +0000

    Fix dev_shutdown script

--- local-development/dev_shutdown.sh
+++ local-development/dev_shutdown.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 set -e
 
-echo "Going into root mode to delete some docker volumes"
-sudo echo "Root mode: OK"
 docker compose down
-#sudo rm -f data/ambar-emulator/queues/inventory.lock
-#sudo rm -f data/ambar-emulator/queues/**/*.lock
-sudo rm -Rf data/ambar-emulator/
-docker compose up -d --build --force-recreate
-sleep 5
