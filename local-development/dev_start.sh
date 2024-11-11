8a1b4a4b54b9d93f7ab3089abec0b9f5a0387fd0
Luis Galeas <galeaspablo@hotmail.com>
2024-11-11 06:09:12 +0000

    Update docs

--- local-development/dev_start.sh
+++ local-development/dev_start.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 set -e
 
-echo "Going into root mode to delete some docker volumes"
+echo "Going into root mode to delete docker volumes"
 sudo echo "Root mode: OK"
 docker compose down
-#sudo rm -f data/ambar-emulator/queues/inventory.lock
-#sudo rm -f data/ambar-emulator/queues/**/*.lock
+
+# Ambar won't have to be recreated from scratch once we fix a bug.
+# For now recreating from scratch means that all events are re-sent upon startup.
 sudo rm -Rf data/ambar-emulator/
+
 docker compose up -d --build --force-recreate
 sleep 5
+
+echo "The application is up!"
+echo "You can navigate to localhost:8080 to view your application."
+echo "If it's your first time, try signing up, verifying your email, and signing in."
+echo "After you've tried this, explore the code using what you learned in Ambar's course!"
