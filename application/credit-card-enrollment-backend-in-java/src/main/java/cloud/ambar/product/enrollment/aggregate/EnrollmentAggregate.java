08c357fd5c8b299c865a49192812a5720927f27b
Tom Schutte <tom@ambar.cloud>
2024-11-12 08:49:12 -0600

    Initial enrollment agg

--- application/credit-card-enrollment-backend-in-java/src/main/java/cloud/ambar/product/enrollment/aggregate/EnrollmentAggregate.java
+++ application/credit-card-enrollment-backend-in-java/src/main/java/cloud/ambar/product/enrollment/aggregate/EnrollmentAggregate.java
@@ -1,4 +1,29 @@
 package cloud.ambar.product.enrollment.aggregate;
 
-public class EnrollmentAggregate {
+import cloud.ambar.common.aggregate.Aggregate;
+import cloud.ambar.common.event.models.Event;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@NoArgsConstructor
+public class EnrollmentAggregate extends Aggregate {
+    private static final Logger log = LogManager.getLogger(EnrollmentAggregate.class);
+
+    public EnrollmentAggregate(String aggregateId) {
+        super(aggregateId);
+    }
+
+    @Override
+    public void transform(Event event) {
+        ObjectMapper om = new ObjectMapper();
+        switch(event.getEventName()) {
+
+        }
+    }
 }
