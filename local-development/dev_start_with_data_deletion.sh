608b21646f6276f0b430c03afb587196afcafe3f
Luis Galeas <galeaspablo@hotmail.com>
2024-11-12 22:29:20 +0000

    Update startup scripts

--- local-development/dev_start_with_data_deletion.sh
+++ local-development/dev_start_with_data_deletion.sh
@@ -4,8 +4,6 @@ set -e
 echo "Going into root mode to delete some docker volumes"
 sudo echo "Root mode: OK"
 docker compose down
-sleep 5
 sudo rm -Rf data/*
-sleep 4
 docker compose up -d --build --force-recreate
-sleep 5
+echo "The application is up!"
