e36782f6f062ca1b146d15984e4cd965bae82d5f
Luis Galeas <galeaspablo@hotmail.com>
2024-11-13 14:18:44 +0000

    Fix mongo health at startup due to failed key

--- local-development/dev_start.sh
+++ local-development/dev_start.sh
@@ -2,7 +2,7 @@
 set -e
 
 docker compose down
-docker compose up -d --build --force-recreate || docker logs mongo-projection
+docker compose up -d --build --force-recreate
 
 echo "The application is up!"
 echo "You can navigate to localhost:8080 to view your application."
