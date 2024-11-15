9be6ef41544f4401890e5f761e0bfd9f42e83637
Luis Galeas <galeaspablo@hotmail.com>
2024-11-15 13:41:29 +0000

    Receive further instructions after application starts

--- local-development/dev_start.sh
+++ local-development/dev_start.sh
@@ -6,5 +6,4 @@ docker compose up -d --build --force-recreate
 
 echo "The application is up!"
 echo "You can navigate to localhost:8080 to view your application."
-echo "If it's your first time, try signing up, verifying your email, and signing in."
-echo "After you've tried this, explore the code using what you learned in Ambar's course!"
+echo "You will receive further instructions in the top menu."
