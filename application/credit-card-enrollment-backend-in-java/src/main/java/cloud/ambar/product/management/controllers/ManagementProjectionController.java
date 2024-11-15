8275b048f5162709c6044e3dfd4ac90aca63cccd
Tom Schutte <tom@ambar.cloud>
2024-11-15 14:06:19 -0600

    Debug

--- application/credit-card-enrollment-backend-in-java/src/main/java/cloud/ambar/product/management/controllers/ManagementProjectionController.java
+++ application/credit-card-enrollment-backend-in-java/src/main/java/cloud/ambar/product/management/controllers/ManagementProjectionController.java
@@ -5,9 +5,9 @@ import cloud.ambar.common.ambar.response.AmbarResponse;
 import cloud.ambar.common.projection.ProjectionController;
 import cloud.ambar.product.management.projection.ProductManagementProjectionService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
 
 /**
  * This controller is responsible for updating the read models from events written to the eventstore (postgre).
@@ -17,16 +17,14 @@ import org.springframework.web.bind.annotation.RestController;
  * This is the Projection/Reaction side of our application
  * Note: This service does not write any new events in response to incoming events, and thus does not have a reaction portion
  */
-@RestController
+@Controller
 @RequiredArgsConstructor
 public class ManagementProjectionController extends ProjectionController {
 
     private final ProductManagementProjectionService productManagementProjectionService;
 
-    @PostMapping(value = "/api/v1/credit_card_product/product/projection",
-            consumes = MediaType.APPLICATION_JSON_VALUE,
-            produces = MediaType.APPLICATION_JSON_VALUE)
-    public AmbarResponse handleEvent(AmbarEvent ambarEvent) {
+    @PostMapping(value = "/api/v1/credit_card_product/product/projection")
+    public AmbarResponse handleEvent(@RequestBody AmbarEvent ambarEvent) {
         return processEvent(ambarEvent, productManagementProjectionService);
     }
 }
