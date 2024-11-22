3e2e5fe1b42a172b801ad79b5e54d4d93a2055b6
Tom Schutte <tom@ambar.cloud>
2024-11-22 10:33:54 -0600

    Card randomizer

--- application/credit-card-enrollment-backend-in-java/src/main/java/cloud/ambar/product/management/util/DefaultCardCreator.java
+++ application/credit-card-enrollment-backend-in-java/src/main/java/cloud/ambar/product/management/util/DefaultCardCreator.java
@@ -1,12 +1,21 @@
 package cloud.ambar.product.management.util;
 
 import cloud.ambar.product.management.commands.ProductManagementCommandService;
+import cloud.ambar.product.management.commands.models.ActivateCreditCardProductCommand;
+import cloud.ambar.product.management.commands.models.DeactivateCreditCardProductCommand;
+import cloud.ambar.product.management.projection.models.CreditCardProduct;
+import cloud.ambar.product.management.query.ProductManagementQueryService;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.RequiredArgsConstructor;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 import static cloud.ambar.product.management.util.Constants.BASIC;
 import static cloud.ambar.product.management.util.Constants.STARTER;
@@ -25,17 +34,62 @@ public class DefaultCardCreator implements ApplicationRunner {
 
     private final ProductManagementCommandService commandService;
 
+    private final ProductManagementQueryService queryService;
+
     @Override
     public void run(ApplicationArguments args) throws Exception {
         log.info("Defining initial card products");
-        try {
-            // Sleep to give time for the db to init.
-            Thread.sleep(15000);
-        } catch (InterruptedException e) {
-            // Do nothing.
-        }
+        sleep(15000);
+
         commandService.handle(BASIC);
         commandService.handle(BASIC_CASH_BACK);
         commandService.handle(BASIC_POINTS);
+
+        log.info("Listing all cards to randomly activate and deactivate them...!");
+        List<CreditCardProduct> products = queryService.getAllCreditCardProducts();
+
+        Random random = new Random();
+        // Generate a random duration between 5 and 10 minutes (in milliseconds)
+        long minMillis = TimeUnit.MINUTES.toMillis(5);
+        long maxMillis = TimeUnit.MINUTES.toMillis(10);
+        long randomMillis;
+
+        products.forEach(p -> {
+            if (!p.isActive()) {
+                try {
+                    commandService.handle(new ActivateCreditCardProductCommand(p.getId()));
+                } catch (JsonProcessingException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+
+        while (true) {
+            products = queryService.getAllCreditCardProducts();
+            randomMillis = minMillis + (long) (random.nextDouble() * (maxMillis - minMillis));
+            sleep(randomMillis);
+
+            CreditCardProduct card = products.get(random.nextInt(products.size()));
+            log.info("Selected card " + card.getName());
+            log.info("Flipping active status.");
+            if (card.isActive()) {
+                try {
+                    commandService.handle(new DeactivateCreditCardProductCommand(card.getId()));
+                } catch (Exception e) {
+                    // We might get an exception back if it was the last active card. We can just move on.
+                }
+            } else {
+                commandService.handle(new ActivateCreditCardProductCommand(card.getId()));
+            }
+        }
+    }
+    private void sleep(long millis) {
+        log.info("Sleeping for " + millis + " milliseconds.");
+        try {
+            // Sleep to give time for the db to init.
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            // Do nothing.
+        }
     }
 }
