69a96efef7948ccf62f94ee5f81ab09e03ee6e0a
Luis Pablo Galeas Bardales <galeaspablo@hotmail.com>
2024-11-30 22:11:23 +0000

    Rename files in backend php

--- application/backend-php/code/src/Console/DefineCreditCardProducts.php
+++ application/backend-php/code/src/Console/DefineCreditCardProducts.php
@@ -15,7 +15,7 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class DefineCreditCards extends Command
+class DefineCreditCardProducts extends Command
 {
     private DefineProductHandler $defineProductHandler;
 
