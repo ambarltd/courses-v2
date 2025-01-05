55652ad43b91a8e53f349ede4fcb2e20d42c8c7d
Luis Pablo Galeas Bardales <galeaspablo@hotmail.com>
2025-01-05 20:38:33 +0000

    Tidy up location of scripts for different OSes and update README

---
+++ local-development/scripts/mac/dev_start.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -e
+
+docker compose down
+docker compose up -d --build --force-recreate
+
+all_services_fully_healthy() {
+    ! docker compose ps --format "table {{.ID}}\t{{.Name}}\t{{.Status}}" | grep -q -E "(unhealthy|starting)"
+}
+
+while ! all_services_fully_healthy; do
+    echo "Waiting for all services to be healthy..."
+    docker compose ps --format "table {{.ID}}\t{{.Name}}\t{{.Status}}"
+    echo ""
+    sleep 5
+done
+
+docker compose ps --format "table {{.ID}}\t{{.Name}}\t{{.Status}}"
+
+echo ""
+echo "=================================================================="
+echo "||                   All services are healthy!                  ||"
+echo "=================================================================="
+echo ""
+
+echo "=================================================================="
+echo "|| You can navigate to localhost:8080 to view your application. ||"
+echo "=================================================================="
+echo "||    You will receive further instructions in the top menu.    ||"
+echo "=================================================================="
